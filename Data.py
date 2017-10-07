5f086a1b72ab07021375910fc95ae4631f731bb6
Dahya <cetk16016@g.nihon-u.ac.jp>
2017-10-07 20:59:12 +0900

    UPDATE

--- Data.py
+++ Data.py
@@ -11,20 +11,23 @@ class Data(object):
     """
     def __init__(self, graph, bandwidth_max, signal_traffic_max, signal_division):
         self.graph = graph
-        self.bandwidth_max = bandwidth_max
-        self.signal_traffic_max=signal_traffic_max
-        self.signal_division=signal_division
+        self.bandwidth_max      = bandwidth_max
+        self.signal_traffic_max = signal_traffic_max
+        self.signal_division    = signal_division
+        self.signal_unit        = signal_traffic_max / signal_division
+        self.signal_now         = self.signal_unit
 
-        self.links_x = self.graph.make_link_list()
-        self.sites_x = self.make_site_link_dic()
-        self.solve_x = self.make_link_matrix() 
-    
+        self.links_x     = self.graph.make_link_list()
+        self.sites_x     = self.make_site_link_dic()
+        self.sites_x_tmp = self.make_site_link_dic()
+        self.solve_x     = self.make_link_matrix() 
+
     def make_link_matrix(self):
         """
         リンクのマトリックスを生成する．
         """
         dic = {(i, j): self.bandwidth_max for i in range(self.graph.node_num) for j in range(self.graph.node_num)}
-        for index in self.graph.make_both_link_list:
+        for index in self.graph.make_both_link_list():
             dic[index] = 0
         
         return dic
@@ -33,10 +36,10 @@ class Data(object):
         """
         拠点ごとのリンク帯域のディクショナリを生成する．
         """
-        dic = {k: v for k in self.graph.site_list for v in self.graph.link_list}
+        dic = {k: self.make_link_matrix() for k in self.graph.site_list}
         return dic
-         
 
+# FOR TEST
 
 if __name__ == '__main__':
     import Network as nw
