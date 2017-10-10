5757575e1e4b1cd67a4512ac073e8e6edaf1575a
Dahya <cetk16016@g.nihon-u.ac.jp>
2017-10-10 16:23:10 +0900

    MAKE！

---
+++ simulationRunner.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# IGNORE RULES OF PYLINT !
+# pylint: disable=C0103
+# pylint: disable=C0301
+# pylint: disable=C0326
+
+import Simulation as Simu
+import Network
+import Data
+import LpModel
+
+class Runner(object):
+    """
+    シミュレーションを実行し，データを生成するクラス．
+    """
+    def __init__(self, o_data, o_model):
+        self.DATA  = o_data
+        self.MODEL = o_model
+        self.SIMU  = Simu.Simulation(o_data, o_model)
+
+    def run(self):
+        end = self.DATA.try_num
+        now = self.DATA.try_now
+        
+        while now < end:
+            print("now:{}, end{}".format(now, end) )
+            self.SIMU.solve()
+            now += 1
+        print self.DATA.std_dev 
+
+
+if __name__ == '__main__':
+    node     = 100
+    site     = 10
+    connect  = 3
+    link_max = 20 * 1024 * 1024
+    sig_max  = 4 * 20000
+    sig_div  = 20
+    vm_add   = 20
+
+    graph  = Network.Topology(node, site, connects=3)
+    data   = Data.Data(graph, link_max, sig_max, sig_div, vm_add)
+    model  = LpModel.Model(data)
+    runner = Runner(data,model)
+    runner.run()
