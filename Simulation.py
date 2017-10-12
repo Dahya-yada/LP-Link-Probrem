28a76faafc78954495d082de039c075b5a2085d7
Dahya <cetk16016@g.nihon-u.ac.jp>
2017-10-12 17:19:09 +0900

    取り敢えずのところまで

--- Simulation.py
+++ Simulation.py
@@ -18,19 +18,28 @@ class Simulation(object):
         self.Data  = o_data
         self.Model = o_model
 
-        self.x_sig        = {s: {l: 0 for l in o_graph.link_list} for o_graph.site_list}
-        self.x_sig_solve  = {s: {l: 0 for l in o_graph.link_list} for o_graph.site_list}
-        self.x_sig_result = {s: {l: 0 for l in o_graph.link_list} for o_graph.site_list}
+        self.x_sig        = {s: {l: 0 for l in o_graph.link_list} for s in o_graph.site_list}
+        self.x_sig_solve  = {s: {l: 0 for l in o_graph.link_list} for s in o_graph.site_list}
+        self.x_sig_result = {s: {l: 0 for l in o_graph.link_list} for s in o_graph.site_list}
         self.x_sig_matrix = {(i, j): 0 for i in range(o_graph.node_num) for j in range(o_graph.node_num)}
 
         self.sig_lb_tmp = self.Data.signal_lb
+        self.add_site   = 0
 
 
     def solve(self):
         """
         1回だけすべての拠点に対し，線形計画法を解く
         """
-        pass
+        self.change_sig_bandwidth()
+        self.make_x_sig_solve()
+        for s in self.Graph.site_list:
+            self.make_x_sig_matrix()
+            self.Model.make_model(s, self.x_sig_matrix)
+            self.Model.MODEL.optimize()
+        self.decide_site()
+        self.update_x_sig(self.add_site)
+        self.update_data_x_sig()
     
     def change_sig_bandwidth(self):
         """
@@ -70,8 +79,50 @@ class Simulation(object):
         tmp = {(i, j): self.Data.bandwidth_max for i in  for n for j in n}
 
         for l in self.Graph.link_list:
-            tmp[l[0], l[1]] = self.x_sig_solve[s][l[0], l[1]]
-            tmp[l[1], l[0]] = self.x_sig_solve[s][l[1], l[0]]
+            tmp[l[0], l[1]] = int(self.x_sig_solve[s][l[0], l[1]])
+            tmp[l[1], l[0]] = int(self.x_sig_solve[s][l[1], l[0]])
+        self.x_sig_matrix = copy.deepcopy(tmp)
+    
+    def update_result_x(self, s):
+        """
+        最適化結果をx_sig_solveとx_sig_resultに反映します．
+        """
+        for l in self.Graph.link_list:
+            for d in self.Graph.link_list:
+                self.x_sig_solve[s][l]  += self.Model.X[d, l[0], l[1]].x
+                self.x_sig_solve[s][l]  += self.Model.X[d, l[1], l[0]].x
+                self.x_sig_result[s][l] += self.Model.X[d, l[0], l[1]].x
+                self.x_sig_result[s][l] += self.Model.X[d, l[1], l[0]].x
+    
+    def decide_site(self):
+        """
+        最適化結果からVMを追加する拠点を決定する．
+        """
+        std_dev = {s: numpy.std(self.x_sig_solve[s].values()) for s in self.Graph.site_list}
+        d_site  = min(std_dev.items(), key=lambda x: x[1])[0]
+        self.Data.std_dev[self.Data.try_now] = std_dev[d_site]
+        self.add_site = d_site
+    
+    def update_x_sig(self, s):
+        """
+        決定した拠点からの仮想リンク帯域をx_sigに反映する．
+        """
+        for l in self.Graph.site_list:
+            self.x_sig[s][l] += self.x_sig_result[s][l]
+        self.x_sig_result = {s: {l: 0 for l in o_graph.link_list} for s in o_graph.site_list}
+
+    def update_data_x_sig(self):
+        tmp = {l: 0 for l in self.Graph.link_list}
+        
+        for s in self.Graph.site_list:
+            for l in self.Graph.site_list:
+                tmp[[l] += self.x_sig[s][l]
+        
+        self.Data.x_sig = copy.deepcopy(tmp)
+
+
+
+
 
 
             
